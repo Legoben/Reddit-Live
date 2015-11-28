6dd9818dd18f3b4c310e621148966646137ab7d3
Ben <legoben1998@gmail.com>
2015-11-27 20:53:14 -0600

    update

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,23 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/com.ququplay.websocket.WebSocket/www/phonegap-websocket.js",
+        "id": "com.ququplay.websocket.WebSocket.websocket",
+        "clobbers": [
+            "WebSocket"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.0",
+    "com.ququplay.websocket.WebSocket": "0.1.0"
+}
+// BOTTOM OF METADATA
+});
