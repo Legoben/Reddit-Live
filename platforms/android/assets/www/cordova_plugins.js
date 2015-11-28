c0f600c889f6c24e3114315aae2e0e737b8bc833
Ben <legoben1998@gmail.com>
2015-11-27 21:11:00 -0600

    update

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -11,13 +11,21 @@ module.exports = [
         "clobbers": [
             "WebSocket"
         ]
+    },
+    {
+        "file": "plugins/com.phonegap.plugins.PushPlugin/www/PushNotification.js",
+        "id": "com.phonegap.plugins.PushPlugin.PushNotification",
+        "clobbers": [
+            "PushNotification"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "cordova-plugin-whitelist": "1.2.0",
-    "com.ququplay.websocket.WebSocket": "0.1.0"
+    "com.ququplay.websocket.WebSocket": "0.1.0",
+    "com.phonegap.plugins.PushPlugin": "2.5.0"
 }
 // BOTTOM OF METADATA
 });
--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -1,49 +1,145 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+document.addEventListener('deviceready', init, false);
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+var ws;
+var current = null;
 
-        console.log('Received Event: ' + id);
+function chars_decode(string, quote_style) {
+  //       discuss at: http://phpjs.org/functions/htmlspecialchars_decode/
+
+  var optTemp = 0,
+    i = 0,
+    noquotes = false;
+  if (typeof quote_style === 'undefined') {
+    quote_style = 2;
+  }
+  string = string.toString()
+    .replace(/&lt;/g, '<')
+    .replace(/&gt;/g, '>');
+  var OPTS = {
+    'ENT_NOQUOTES': 0,
+    'ENT_HTML_QUOTE_SINGLE': 1,
+    'ENT_HTML_QUOTE_DOUBLE': 2,
+    'ENT_COMPAT': 2,
+    'ENT_QUOTES': 3,
+    'ENT_IGNORE': 4
+  };
+  if (quote_style === 0) {
+    noquotes = true;
+  }
+  if (typeof quote_style !== 'number') { // Allow for a single string or an array of string flags
+    quote_style = [].concat(quote_style);
+    for (i = 0; i < quote_style.length; i++) {
+      // Resolve string input to bitwise e.g. 'PATHINFO_EXTENSION' becomes 4
+      if (OPTS[quote_style[i]] === 0) {
+        noquotes = true;
+      } else if (OPTS[quote_style[i]]) {
+        optTemp = optTemp | OPTS[quote_style[i]];
+      }
     }
-};
+    quote_style = optTemp;
+  }
+  if (quote_style & OPTS.ENT_HTML_QUOTE_SINGLE) {
+    string = string.replace(/&#0*39;/g, "'"); // PHP doesn't currently escape if more than one 0, but it should
+    // string = string.replace(/&apos;|&#x0*27;/g, "'"); // This would also be useful here, but not a part of PHP
+  }
+  if (!noquotes) {
+    string = string.replace(/&quot;/g, '"');
+  }
+  // Put this in last place to avoid escape being double-decoded
+  string = string.replace(/&amp;/g, '&');
+
+  return string;
+}
+
+function getThreads(){
+    $.ajax({url:"https://www.reddit.com/r/live/.json", success: function(data){
+        var j = data.data.children;
+        for(var i = 0; i < j.length; i++){
+            if (j[i].data.secure_media == null || j[i].data.secure_media.type != "liveupdate"){
+                continue;
+            }
+            
+            var lid = j[i].data.secure_media.event_id;
+            var title = j[i].data.title;
+            
+            console.log("LIVE THREAD" + lid + title);
+            $("#sel").append("<option value='"+lid+"'>"+title+"</option>")
+            
+        }
+        
+        $("#sel option[value=null]").html("SELECT")
+    }})
+}
+
+function connectToWS(lid){
+    $.ajax({url:"https://www.reddit.com/live/"+lid+"/about.json", success: function(j){
+        ws = new WebSocket(j.data.websocket_url)
+        ws.onmessage = wsUpdate;
+        ws.onclose = function(){
+            console.log("CLOSED")
+            connectToWS(lid);   
+        }
+        
+    }})
+}
+
+function wsUpdate(event){
+    console.log("GOT EVENT")
+    console.log(event.data);
+}
+
+function renderUpdate(j){
+    var ele = $('<li class="list-group-item" id="up-'+j.data.id+'"></li>');
+    console.log("j " + JSON.stringify(j));
+    var body = chars_decode(j.data.body_html);
+    console.log("BODY "+body);
+    $(ele).prepend(body);
+    
+    if(j.data.embeds.length != 0){
+        for(var i = 0; i < j.data.embeds.length; i++){
+            
+          $(ele).append("<iframe class='embed' width='100%' src='"+j.data.embeds[i].url+"' />")  
+            
+        }
+    }
+    
+    //ToDo: format time
+    $(ele).append("<i class='authtime'>/u/"+j.data.author+" at "+j.data.created+"</i>")
+    
+    $("#updates").append(ele);
+}
+
+function openThread(lid){
+    if(current !=  null){
+        closeThread()
+    }
+    
+    $.ajax({url:"https://api.reddit.com/live/"+lid+"?limit=40", success: function(data){
+        var j = data.data.children;
+        for(var i = 0; i < j.length; i++){
+            renderUpdate(j[i])   
+        }
+            
+    }})
+    
+    connectToWS(lid);
+       
+}
+
+function closeThread(){
+       
+}
+
+function init(){
+    console.log("DEVICE READY");
+    getThreads();
+    
+}
+
+$('#sel').on('change', function() {
+    var lid = $(this).val();
+    
+    var lid = "vzpz90hf0c0c"; //TODO: REMOVE
+    
+    openThread(lid);
+});
